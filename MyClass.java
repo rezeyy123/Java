45dda2f6125b08c7e7051720feda97552b5a6814
rezeyy123 <piotrkur2009@gmail.com>
2026-02-09 09:53:40 +0000

    vnbmvbnm

--- MyClass.java
+++ MyClass.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class MyClass {
-  public static void main(String args[]) {
+public static void main(String args[]) {
 
-    Scanner sc = new Scanner(System.in);
+  Scanner sc = new Scanner(System.in);
     System.out.print("Steigung m eingeben: ");
     float m = sc.nextFloat();
     System.out.print("y-Achsenabschnitt b eingeben: ");
     float b = sc.nextFloat();
     float nullstelle=-b/m;
 
- System.out.println( "Die Nullstelle der linearen Funktion f(x) = " + m + "x + " + b + " ist: " + nullstelle);
-  }
+    System.out.println( "Die Nullstelle der linearen Funktion f(x) = " + m + "x + " + b + " ist: " + nullstelle);
+}
 }
