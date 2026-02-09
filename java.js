3b9163e5e914f570da9743a1cb95fcab64620757
rezeyy123 <piotrkur2009@gmail.com>
2026-02-09 09:15:05 +0000

    gfhfgh

---
+++ java.js
@@ -0,0 +1,9 @@
+public class MyClass {
+  public static void main(String args[]) {
+float a=7.43f;
+float b=5.09f;
+float A=a*b;
+
+ System.out.println( "Der Flächeninhalt von a*b  = " + A);
+  }
+}
