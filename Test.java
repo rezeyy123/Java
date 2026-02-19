ceb96f9113027aa5813ce7f797b6d14e044ca1a9
rezeyy123 <piotrkur2009@gmail.com>
2026-02-19 12:01:07 +0000

    -.

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+		
+public static int verdopple (int zahl) { 
+	return zahl * 2; 
+}
+
+public static void main(String[] args) {
+	int a = 5;
+	System.out.println("Verdoppelt von " + a + " ist :" + verdopple(a));
+}
+}
